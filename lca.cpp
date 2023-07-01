4845d3629c75c8ed58f55c2fd5dee28cd18e4628
dh-gntr <96826884+dh-gntr@users.noreply.github.com>
2023-07-01 18:15:43 +0530

    Create lca.cpp

---
+++ lca.cpp
@@ -0,0 +1,20 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(root==NULL)
+        return NULL;
+        if(root==p || root==q)
+        return root;
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left==NULL && right==NULL)
+        return NULL;
+        if(left!=NULL && right!=NULL)
+        return root;
+        if(left)
+        return left;
+        else
+        return right;
+
+
+        
+    }
